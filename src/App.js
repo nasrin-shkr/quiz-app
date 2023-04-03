39711f8258728d23b6a0375f7c371747c031a4e6
NasrinShaker <nasrin.shaker.ar@gmail.com>
2023-04-03 19:39:59 +0330

    sample commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     // <div className="App">
     <div className="bg-green-400 flex flex-col items-center justify-center h-screen">
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Quiz />
     </div>
   );
